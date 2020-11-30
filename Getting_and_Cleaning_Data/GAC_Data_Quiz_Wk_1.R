ecc5411051405a832100115b87cd551755e1dc9f
Angel G <argonzalez82@gmail.com>
2020-11-30 16:30:18 -0700

    Auto stash before merge of "master" and "origin/master"

--- Getting_and_Cleaning_Data/GAC_Data_Quiz_Wk_1.R
+++ Getting_and_Cleaning_Data/GAC_Data_Quiz_Wk_1.R
@@ -4,32 +4,32 @@
   
     
     
-#checking current working directory
-#"/home/CentOS_Laptop/datasciencecoursera/Getting_and_Cleaning_Data"
-    getwd()
+  #checking current working directory
+  #"/home/CentOS_Laptop/datasciencecoursera/Getting_and_Cleaning_Data"
+      getwd()
 
-#creating a new folder and file to store the imported data
-    dir.create("data")
+  #creating a new folder and file to store the imported data
+      dir.create("data")
 
-#setting the working directory to the new data directory to place
-#the new .csv in the directory.
-#Creating function since the steps in line 15 - 25
-#will be needed multiple times in other questions in the Quiz.   
+  #setting the working directory to the new data directory to place
+  #the new .csv in the directory.
+  #Creating function since the steps in line 15 - 25
+  #will be needed multiple times in other questions in the Quiz.   
     
-createFile <- function(fileName = "2006_m!icrodata_survey.csv" ) {
-    setwd("/home/CentOS_Laptop/datasciencecoursera/Getting_and_Cleaning_Data/data")
+    createFile <- function(fileName = "2006_m!icrodata_survey.csv" ) {
+        setwd("/home/CentOS_Laptop/datasciencecoursera/Getting_and_Cleaning_Data/data")
     
     file.create(fileName)
 
-#checkingfile in directory
+  #checkingfile in directory
     print(list.files("./data"))
 
-#setting directory back to main directory 
+  #setting directory back to main directory 
     setwd("/home/CentOS_Laptop/datasciencecoursera/Getting_and_Cleaning_Data")
 }
 
-#Question 1:  Download from fileURL object into a .csv in the data dir.
-# http address is assigned to the fileURL object
+  #Question 1:  Download from fileURL object into a .csv in the data dir.
+  # http address is assigned to the fileURL object
     
     fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
     download.file(fileURL, 
@@ -37,8 +37,8 @@ createFile <- function(fileName = "2006_m!icrodata_survey.csv" ) {
                 method ="curl")
 
 
-#reading the survey data into R /
-#creating a table 
+  #reading the survey data into R /
+  #creating a table 
 
     surveyData <- read.table("./data/2006_microdata_survey.csv", 
                             sep = ",", 
@@ -46,11 +46,11 @@ createFile <- function(fileName = "2006_m!icrodata_survey.csv" ) {
     
     head(surveyData)
     
-#calculate how many houses cost over $1,000,000
-#variable definitions are in the codebook in the address below
-#performed the same steps as line 13 -20 using createFile function 
-#except creating a new .pdf
-#file for the code  
+  #calculate how many houses cost over $1,000,000
+  #variable definitions are in the codebook in the address below
+  #performed the same steps as line 13 -20 using createFile function 
+  #except creating a new .pdf
+  #file for the code  
     
     createFile(fileName = "2006_survey_codeBook.pdf")
     
@@ -60,12 +60,19 @@ createFile <- function(fileName = "2006_m!icrodata_survey.csv" ) {
                 destfile = "./data/2006_survey_codeBook.pdf",
                 method ="curl")
 
+<<<<<<< Updated upstream
 #housing cost, are in the variable labeled VAL , and 
 #value #24 is for homes over $1,000,000
 # Total sum is = 53 for homes in this price$ range   
 print(homes_values_over_a_mil <- sum(na.exclude(surveyData$VAL > 23)))
+=======
+  #housing cost, are in the variable labeled VAL , and 
+  #value #24 is for homes over $1,000,000
+  # Total sum is = 53 for homes in this price$ range   
+    homes_values_over_a_mil <- sum(na.exclude(surveyData$VAL > 23))
+>>>>>>> Stashed changes
 
-homes_values_over_a_mil
+    homes_values_over_a_mil
 
 
 
@@ -73,10 +80,10 @@ homes_values_over_a_mil
 ###################################
 
 
-#Question 3 - Download and Load 
-#Excel spreadsheet on Natural Gas Aquisition Program
-#performed the same steps as line 13 -20 using the createFile function. 
-#to create a new .xlsx file in data and switch back the working directory
+  #Question 3 - Download and Load 
+  #Excel spreadsheet on Natural Gas Aquisition Program
+  #performed the same steps as line 13 -20 using the createFile function. 
+  #to create a new .xlsx file in data and switch back the working directory
 
     createFile(fileName = "NaturalGasAquisitionProgram.xlsx")
 
@@ -85,9 +92,9 @@ homes_values_over_a_mil
     download.file(NatGasProgramURL,
               destfile = "./data/NaturalGasAquisitionProgram.xlsx",
               method ="curl")
-    
-#"Read rows 18-23 and columns 7-15 into R and 
-#assign the result to a variable called: dat"
+      
+  #"Read rows 18-23 and columns 7-15 into R and 
+  #assign the result to a variable called: dat"
     
     install.packages("xlsx")    
     library(xlsx)
@@ -99,94 +106,95 @@ homes_values_over_a_mil
                      rowIndex = rowIndex,
                      colIndex = colIndex)
     
-#second part of Question 3:
-   zipsum <- sum(dat$Zip*dat$Ext,na.rm = T)
+  #second part of Question 3:
+    zipsum <- sum(dat$Zip*dat$Ext,na.rm = T)
 
     
    
    
-#Question 4
-#Read the XML data on Baltimore restaurants from here:
-#https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml
-#How many restaurants have zipcode 21231?
+  #Question 4
+  #Read the XML data on Baltimore restaurants from here:
+  #https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml
+  #How many restaurants have zipcode 21231?
    
-  install.packages("XML", "RCurl")
-  library(XML, RCurl)
+    install.packages("XML", "RCurl")
+    library(XML, RCurl)
   
-#Assign the XML website to variable xmlURL
-  xmlURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+  #Assign the XML website to variable xmlURL
+    xmlURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
   
-#parse the xml and assign to doc variable use the sub() to remove "s" from https
-#since xml doesnt work https sites. 
-  doc <- xmlTreeParse(sub("s", "", xmlURL), useInternal=TRUE)
-  rootNode <- xmlRoot(doc)
-  print(rootNode)
+  #parse the xml and assign to doc variable use the sub() to remove "s" from https
+  #since xml doesnt work https sites. 
+    doc <- xmlTreeParse(sub("s", "", xmlURL), useInternal=TRUE)
+    rootNode <- xmlRoot(doc)
+    print(rootNode)
   
-#working on collecting zipcode node in order to complete the solution
-#1st attempt - outcome not really useable yet 
-  zipCollect <- xpathApply(doc = rootNode, "//zipcode" )
-  print(zipCollect)
+  #working on collecting zipcode node in order to complete the solution
+  #1st attempt - outcome not really useable yet 
+    zipCollect <- xpathApply(doc = rootNode, "//zipcode" )
+    print(zipCollect)
 
-#2nd attempt outcome is useable     
-  zipCollect2 <- xpathSApply(rootNode, "//zipcode[contains(text(), 
+  #2nd attempt outcome is useable     
+    zipCollect2 <- xpathSApply(rootNode, "//zipcode[contains(text(), 
                              '21231')]", xmlValue) 
   
   
-#installing plyr package to use the count function plyr::count
-  install.packages("plyr")
-  library(plyr)
+  #installing plyr package to use the count function plyr::count
+    install.packages("plyr")
+    library(plyr)
 
-#zipCollect2 is Character / changed to integer to count the 
+  #zipCollect2 is Character / changed to integer to count the 
   
-  zipCount <- count(zipCollect2)
+    zipCount <- count(zipCollect2)
   
 
 
   
-#Question:5
-#The American Community Survey distributes downloadable data 
-#about United States communities. Download the 2006 microdata survey 
-#about housing for the state of 
-#Idaho using download.file() from here:
+  #Question:5
+  #The American Community Survey distributes downloadable data 
+  #about United States communities. Download the 2006 microdata survey 
+  #about housing for the state of 
+  #Idaho using download.file() from here:
   
-#https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv
+  #https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv
   
-#get working directory  / to verify current directory
-  getwd()
+  #get working directory  / to verify current directory
+    getwd()
 
-#Reusing CreateFile function from Question1 
-#creating a new to file in the data folder 
-#to place the download() from the url
+  #Reusing CreateFile function from Question1 
+  #creating a new to file in the data folder 
+  #to place the download() from the url
 
-  createFile(fileName = "2006_microdata_2.csv") 
+    createFile(fileName = "2006_microdata_2.csv") 
   
-#Question 5:  Download from fileURL object into a .csv in the data dir.
-# http address is assigned to the fileURL object
+  #Question 5:  Download from fileURL object into a .csv in the data dir.
+  #http address is assigned to the fileURL object
   
-  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
-  download.file(fileURL, 
+    fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+    download.file(fileURL, 
                 destfile = "./data/2006_microdata_2.csv",
                 method ="curl")
   
 
-#reading the survey data into R /
-#creating a table using fread() 
-#data.table package is needed for the fread()  
+  #reading the survey data into R /
+  #creating a table using fread() 
+  #data.table package is needed for the fread()  
   
-install.packages("data.table") 
-library(data.table)
+    install.packages("data.table") 
+    library(data.table)
 
   
-  DT <- fread("./data/2006_microdata_2.csv", 
-                           sep = ",", 
-                           header =TRUE,
-                           verbose = TRUE)
+    DT <- fread("./data/2006_microdata_2.csv", 
+                                  sep = ",", 
+                                header =TRUE,
+                              verbose = TRUE)
   
-  head(DT)  
+    head(DT)  
 
-#second part of Question5 Testing different solution to 
+  #second part of Question5 Testing different solution to 
 #to find the fastest solution
   
+<<<<<<< Updated upstream
   install.packages("rbenchmark")
   library(rbenchmark)
   
@@ -209,3 +217,20 @@ library(data.table)
   benchmark(Q5_Solution6)
  
  
+=======
+ Q5_Solution1 <- (DT[DT$SEX==1,]$pwgtp15);mean(DT[DT$SEX==2,]$pwgtp15)
+  
+    system.time(Q5_Solution1)
+  
+ Q5_Solution2 <- 
+    system.time(Q5_Solution2) 
+ 
+ Q5_Solution3 <- 
+   system.time(Q5_Solution3) 
+ 
+ Q5_Solution4 <- 
+   system.time(Q5_Solution4) 
+ 
+ Q5_Solution5 <- 
+   system.time(Q5_Solution5) 
+>>>>>>> Stashed changes
--- Getting_and_Cleaning_Data/GAC_Data_Quiz_Wk_2.R
+++ Getting_and_Cleaning_Data/GAC_Data_Quiz_Wk_2.R
@@ -1,1 +1,51 @@
-install.packages("RMySQL")
+#Week 2 Quiz  
+
+
+
+    #Question 1
+    #Register an application with the Github API 
+    #here https://github.com/settings/applications. 
+    #Access the API to get information on your instructors repositories 
+    #(hint: this is the url you want 
+    #"https://api.github.com/users/jtleek/repos"). 
+    #Use this data to find the time that the datasharing repo was created. 
+    #What time was it created?
+    #This tutorial may be useful 
+    #(https://github.com/hadley/httr/blob/master/demo/oauth2-github.r). 
+    #You may also need to run the code in the base R package and not R studio.
+
+
+    #Following work was run in Base R not Rstudio just saving here to save work.     
+    #Need the following package to use  oauth()
+
+    install.packages("curl")
+    install.packages("httr") #may need curl, openssl libraries first. 
+    library(httr)
+    
+    # 1. Find OAuth settings for github:
+    #    http://developer.github.com/v3/oauth/
+    oauth_endpoints("github")
+    
+    # 2. To make your own application, register at
+    #    https://github.com/settings/developers. Use any URL for the homepage URL
+    #    (http://github.com is fine) and  http://localhost:1410 as the callback url
+    #
+    #    Replace your key and secret below.
+    myapp <- oauth_app("github",
+                       key = "56b637a5baffac62cad9",
+                       secret = "8e107541ae1791259e9987d544ca568633da2ebf"
+    )
+    
+    # 3. Get OAuth credentials
+    github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+    
+    # 4. Use API
+    gtoken <- config(token = github_token)
+    req <- GET("https://api.github.com/rate_limit", gtoken)
+    stop_for_status(req)
+    content(req)
+    
+    # OR:
+    req <- with_config(gtoken, GET("https://api.github.com/rate_limit"))
+    stop_for_status(req)
+    content(req)
